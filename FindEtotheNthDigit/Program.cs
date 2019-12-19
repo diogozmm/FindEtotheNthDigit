a951523f6bc72fcd5dc6e5e9d6df513c9efdc55c
Diogo Zimmermann <diogo.zmm@gmail.com>
2019-12-19 10:55:24 -0300

    Code Ready

---
+++ FindEtotheNthDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FindEtotheNthDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor entre 1 e 15");
+
+            try
+            {
+                int value = int.Parse(Console.ReadLine());
+                Console.WriteLine("A Constante E é igual a:");
+                Console.WriteLine(Math.Round(Math.E, value));
+            }
+
+            catch(Exception)
+            {
+                Console.WriteLine("Digite um valor entre 1 e 15!!!");
+            }
+
+        }
+    }
+}
